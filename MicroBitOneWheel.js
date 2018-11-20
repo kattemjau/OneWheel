994c1ddd2836fc568b7c1676cb31eb92eea711b4
kattemjau <sindre.c.h@hotmail.com>
2018-11-20 15:47:56 +0100

    Updated values, PIID tuning done in vesc

--- MicroBitOneWheel.js
+++ MicroBitOneWheel.js
@@ -1,25 +1,40 @@
-let MIN = 1000;
-let MAX = 2000;
+let MIN = -200;
+let MAX = 200;
 let balanced = 1500;
 
+/*
+basic.setMinMax(function(){
+  //let run for 10 secs
+  // tilt board from max left to max right
+  let y = input.acceleration(Dimension.Y);
+  if( y > MAX){
+    MAX=y;
+  }if(y<MIN){
+    MIN=y;
+  }
+
+  control.waitMicros(100);
+})
+*/
 basic.forever(function () {
 
     //if start seqence = done, then
+    // values range from -1023 to 1023
     let y = input.acceleration(Dimension.Y);
-    y = Math.map(y, -1023, 1023, 1000, 2000);
 
-    serial.writeLine("value");
-    serial.writeNumber(y);
+    // serial.writeLine("value");
+    // serial.writeNumber(y);
 
     //if board tutching ground, turn off
-    //if y < MIN{
-    // y=balanced;
-    // }
-    // if y > MAX {
-    // y = balanced;
-    // }
+    if (y < MIN){
+      y=balanced;
+    }else if( y > MAX) {
+      y = balanced;
+    }else{
+      y = Math.map(y, MAX, MIN, 1000, 2000);
+    }
 
-    //add PID controller
+    //add PID controller UPDATE ( USE PID CONTROLLER IN VESC)
     pins.servoSetPulse(AnalogPin.P16, y)
 
     //hm, remove delay?
